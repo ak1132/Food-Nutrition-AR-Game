d4a6707840c7620a8a5e538a6f6bc8c035daec44
DESKTOP-FA6IQA1\Amogh Kulkarni <amogh.kulkarni1993@gmail.com>
2018-11-30 00:47:15 -0500

    Added new script

---
+++ Assets/Scripts/DebugWriter.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.IO;
+
+public class DebugWriter : MonoBehaviour {
+
+    private string sysFilePath;
+
+	void Start () {
+        sysFilePath = Application.persistentDataPath + "/foodLogs.txt";
+
+        if (File.Exists(sysFilePath))
+        {
+            try
+            {
+                File.Delete(sysFilePath);
+                Debug.Log("File deleted");
+            }
+            catch (System.Exception e)
+            {
+                Debug.LogError("Cannot delete the file : "+e.Message);
+            }
+        }
+	}
+	
+	public void WriteToFile(string message)
+    {
+        try
+        {
+            StreamWriter fileWriter = new StreamWriter(sysFilePath, true);
+
+            fileWriter.Write(message+"\n");
+            fileWriter.Close();
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError("Cannot write into the file");
+        }
+        
+    }
+}
